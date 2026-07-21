f34249f6e47abca6476757829e4bb6f673a4a01f
contributor7 <billthemonster1@gmail.com>
2026-07-21 08:49:57 -0600

    Add: merge-k-sorted-linked-lists - submission-1

---
+++ Data Structures & Algorithms/merge-k-sorted-linked-lists/submission-1.py
@@ -0,0 +1,57 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+
+'''
+loop through every list to check their current node to track a valid min value
+
+heap?
+
+one way is just to traverse thorugh every nodee and put its value on aheap,
+then in a for loop create a linkeedlist with value heappopmin
+O(N*M) time complexity
+O(M) space complexity
+
+if you want O(1), then you need to use the original lists
+so just merge two lists until you get no remaining lists
+'''
+class Solution:    
+    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
+        if not lists:
+            return
+
+        def mergeTwoLists(list1, list2):
+            dummy = ListNode()
+            tail = dummy
+            while list1 and list2:
+                if list2.val <= list1.val:
+                    tail.next = list2
+                    list2 = list2.next
+                else:
+                    tail.next = list1
+                    list1 = list1.next
+                tail = tail.next
+            tail.next = list1 or list2
+            return dummy.next
+
+        def printll(head):
+            current = head
+            while current:
+                print(current.val, end=' -> ')
+                current = current.next
+            print('None')
+
+        # printll(mergeTwoLists(lists[0], lists[1]))
+        l = 0
+        r = len(lists) - 1
+        while l < r:
+            mid = (l + r) // 2
+            mergeTwoLists(lists[l], lists[mid])
+            mergeTwoLists(lists[mid], lists[r])
+
+            l += 1
+            r -= 1
+        
+        return lists[l]
