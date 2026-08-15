2812fdee9ccc32fd1b9652e8c87b8f101b29c3d0
contributor7 <billthemonster1@gmail.com>
2026-08-15 13:06:55 -0600

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.py
@@ -0,0 +1,23 @@
+from collections import heapq
+import heapq
+
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        # self.min_val = None
+
+    def push(self, val: int) -> None:
+        if not self.stack:
+            self.stack.append((val, val))
+        else:
+            self.stack.append((val, min(val, self.stack[-1][1])))
+
+    def pop(self) -> None:
+        self.stack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1][0]
+
+    def getMin(self) -> int:
+        return self.stack[-1][1]
